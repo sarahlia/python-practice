22c6c99c38d9f16a6aa9c504b19705907960af08
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-07-02 19:56:12 -0500

    finish first exercise

---
+++ practice.py
@@ -0,0 +1,32 @@
+x = 7
+y = 5.6
+
+total = x * y
+print(round(total,2))
+
+print(type(x))
+print(type(y))
+
+
+"7" + str(7) + "apple"
+
+
+check = 50
+tax_rate = 0.08
+tip_rate = 0.18
+
+tax = check * tax_rate
+tip = check * tip_rate
+total = check + tax + tip
+print("your total is", total))
+
+check = input("How much is your meal ")
+print(check, type(check))
+check = float(check)
+tax_rate = 0.08
+tip_rate = 0.18
+
+tax = check * tax_rate
+tip = check * tip_rate
+total = check + tax + tip
+print("your total is ${:.2f}".format(total))
